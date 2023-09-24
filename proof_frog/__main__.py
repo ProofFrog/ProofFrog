fd251a7e2831657e1a43efc3873fb7c06fd07f96
Qcode <therealrossevans@gmail.com>
2023-09-24 19:19:05 -0400

    Give main "parse" command line arg

--- proof_frog/__main__.py
+++ proof_frog/__main__.py
@@ -11,35 +11,40 @@ from parsing.ProofParser import ProofParser
 from ast_generation import SchemeASTGenerator, PrimitiveASTGenerator, GameASTGenerator, ProofASTGenerator
 
 
-def main(argv: list[str]) -> None:
-    my_ast = None
-    input_stream = FileStream(argv[2])
+def parse(file_type: str, file_name: str) -> None:
+    input_stream = FileStream(file_name)
     lexer_functor: type[PrimitiveLexer] | type[SchemeLexer]
     parser_functor: type[PrimitiveParser] | type[SchemeParser]
     ast_generator: type[PrimitiveASTGenerator] | type[SchemeASTGenerator]
 
-    if argv[1] == 'primitive':
-        lexer_functor = PrimitiveLexer
-        parser_functor = PrimitiveParser
-        ast_generator = PrimitiveASTGenerator
-    elif argv[1] == 'scheme':
-        lexer_functor = SchemeLexer
-        parser_functor = SchemeParser
-        ast_generator = SchemeASTGenerator
-    elif argv[1] == 'game':
-        lexer_functor = GameLexer
-        parser_functor = GameParser
-        ast_generator = GameASTGenerator
-    elif argv[1] == 'proof':
-        lexer_functor = ProofLexer
-        parser_functor = ProofParser
-        ast_generator = ProofASTGenerator
+    match file_type:
+        case 'primitive':
+            lexer_functor = PrimitiveLexer
+            parser_functor = PrimitiveParser
+            ast_generator = PrimitiveASTGenerator
+        case 'scheme':
+            lexer_functor = SchemeLexer
+            parser_functor = SchemeParser
+            ast_generator = SchemeASTGenerator
+        case 'game':
+            lexer_functor = GameLexer
+            parser_functor = GameParser
+            ast_generator = GameASTGenerator
+        case 'proof':
+            lexer_functor = ProofLexer
+            parser_functor = ProofParser
+            ast_generator = ProofASTGenerator
 
     lexer = lexer_functor(input_stream)
     parser = parser_functor(CommonTokenStream(lexer))
     tree = parser.program()
     my_ast = ast_generator().visit(tree)
     print(my_ast)
+
+
+def main(argv: list[str]) -> None:
+    if argv[1] == 'parse':
+        parse(argv[2], argv[3])
 
 
 if __name__ == '__main__':

0d2f06f54b091651d9f545958121f6f297caaf49
Qcode <therealrossevans@gmail.com>
2024-02-06 23:22:03 -0500

    Export main for use in command line scripts

--- proof_frog/__main__.py
+++ proof_frog/__main__.py
@@ -4,16 +4,19 @@ from proof_frog import frog_parser
 from proof_frog import proof_engine
 
 
-def usage(module_name: str) -> None:
+def usage() -> None:
     print("Incorrect Arguments", file=sys.stderr)
-    print(f"Usage: {module_name} parse [primitive|game|scheme|proof] <file>")
-    print(f"Usage: {module_name} prove <file.proof>")
+    print("Usage: proof_frog parse [primitive|game|scheme|proof] <file>")
+    print("Usage: proof_frog prove <file.proof>")
     sys.exit(1)
 
 
 def main() -> None:
     init(autoreset=True)
     argv: list[str] = sys.argv
+    if len(argv) < 2:
+        usage()
+
     if argv[1] == "parse":
         ast_type = argv[2]
         file = argv[3]
@@ -27,12 +30,12 @@ def main() -> None:
             case "proof":
                 print(frog_parser.parse_proof_file(file))
             case _:
-                usage(argv[0])
+                usage()
     elif argv[1] == "prove":
         engine = proof_engine.ProofEngine(argv[2], len(argv) > 3 and argv[3] == "-v")
         engine.prove()
     else:
-        usage(argv[0])
+        usage()
 
 
 if __name__ == "__main__":

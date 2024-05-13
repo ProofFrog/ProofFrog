1934e698d82e8e43845c925648db8507f2f789a2
Qcode <therealrossevans@gmail.com>
2024-05-13 11:04:27 -0400

    Move ill-formed out of examples folder

--- testSemanticAnalysis.sh
+++ testSemanticAnalysis.sh
@@ -4,12 +4,10 @@ skipped="/OTP.scheme$|CounterPRG.scheme|TriplingPRG.scheme$|OFB.scheme$"
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-for file in $(find examples -type f); do
-	echo $file | egrep "ill-formed" > /dev/null
-	isIllFormed=$?
+for file in $(find ill-formed -type f); do
 	echo $file | egrep "fixtures" > /dev/null
 	isFixture=$?
-	if [ $isIllFormed -eq 0 -a $isFixture -ne 0 ]; then
+	if [ $isFixture -ne 0 ]; then
 		echo "Checking $file is ill-formed"
 		python3 -m proof_frog check $file
 		if [ $? -ne 2 ]; then
@@ -17,21 +15,23 @@ for file in $(find examples -type f); do
 			exit 1
 		fi
 		echo
-	else
-		echo $file | egrep "proof|game" > /dev/null
-		if [ $? -eq 0 ]; then
-			continue
-		fi
-		echo $file | egrep $skipped > /dev/null
-		if [ $? -eq 0 ]; then
-			echo -e "${RED}FILE ${file} WAS SKIPPED${NC}"
-			continue
-		fi
-		echo "Checking $file is well-formed"
-		python3 -m proof_frog check $file
-		if [ $? -ne 0 ]; then
-			echo "$file WAS NOT DETECTED AS WELL-FORMED"
-			exit 1
-		fi
+	fi
+done
+
+for file in $(find examples -type f); do
+	echo $file | egrep "proof|game" > /dev/null
+	if [ $? -eq 0 ]; then
+		continue
+	fi
+	echo $file | egrep $skipped > /dev/null
+	if [ $? -eq 0 ]; then
+		echo -e "${RED}FILE ${file} WAS SKIPPED${NC}"
+		continue
+	fi
+	echo "Checking $file is well-formed"
+	python3 -m proof_frog check $file
+	if [ $? -ne 0 ]; then
+		echo "$file WAS NOT DETECTED AS WELL-FORMED"
+		exit 1
 	fi
 done
